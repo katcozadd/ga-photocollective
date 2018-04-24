b50935fad28b208af4d7cc04851dec8c125dd9db
katcozadd <krosecozadd@gmail.com>
2018-04-24 15:35:33 -0600

    fixing heroku probs

--- models/user.js
+++ models/user.js
@@ -1,8 +1,20 @@
 //Reguring mongoose and bcrypt
 const mongoose = require('mongoose');
+if (process.env.NODE_ENV == "production") {
+  console.log("connecting to... " + process.env.NODE_ENV)
+  console.log("also connecting to mlab  " + process.env.MLAB_URL)
+  mongoose.connect(process.env.MLAB_URL)
+} else {
+  console.log("this is the local ")
+  mongoose.connect("mongodb://localhost/colorado-collective");
+}
+
+
 const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
+
+
 
 var UserSchema = new Schema({
   email: String,
